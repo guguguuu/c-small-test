a639181e00449f95c082f42f010d3d21f768dc9a
git <yadochan@qq.com>
2015-12-29 15:57:22 +0100

    modify realloc

--- avg_files/average.c/avg_realloc.c
+++ avg_files/average.c/avg_realloc.c
@@ -7,28 +7,23 @@ int main()
 	int i=0;
  	float sum=0;	
 	int size=5;
-//	float*s=NULL;
+	float*s=NULL;
 
 	printf("how many numbers do you want to input?\n");
 	scanf("%d",&n);
-	float*s=(float*)malloc(sizeof(float)*size);
+	s=(float*)malloc(sizeof(float)*size);
 
 	for(i=0;i<n;i++)
 	{
-			printf("if2 %p\n",s);
-		printf("please input the %d number:",i+1);
-			printf("if5 %p\n",s);
-	//	scanf("%f",&s[i]);
-			printf("if1 %p\n",s);
-		sum+=s[i];
-		s[i]=i;
-		printf("for %p\n",s+i);
 		if(i==size)
 		{		
-			size=size*2;
-			float*s=NULL;	
+			size*=2;
 			s=(float*)realloc(s,sizeof(float)*size);
 		}
+		printf("please input the %d number:",i+1);
+		scanf("%f",&s[i]);
+		sum+=s[i];
+	//	s[i]=i;
 	}
 	float avg=sum/n;
 	printf("sum=%f,avg=%f,n=%d,i=%d\n",sum,avg,n,i);
